a306887c09ab9c6864c34c664d2092aab4f7ad20
WladimirAbdulayev <vl.abdulayev@gmail.com>
2023-02-03 09:57:00 +0100

    configs

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,14 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { KEButton, KECard, Thing } from '../.';
 
 const App = () => {
   return (
     <div>
       <Thing />
+      <KEButton text={'Example Button'}></KEButton>
+      <KECard text={'Example Card'}></KECard>
     </div>
   );
 };
